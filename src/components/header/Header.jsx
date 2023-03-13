8f2909804f1c53e0a4746aaa3b916772254c0958
samuel-satter <97049361+samuel-satter@users.noreply.github.com>
2023-03-13 16:53:02 +0100

    Added info to header section

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css"
+
+const Header = () => {
+    return(
+        <Header>
+            <div className="container header__container"></div>
+            <h5>Hello I'm</h5>
+            <h1>Samuel Sätter</h1>
+            <h5 className="text-light"> Java Developer</h5>
+        </Header>
+    )
+}
